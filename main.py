1413cb59710f642875c32a047ab97646412be71b
varzeny <varzeny@gmail.com>
2024-11-06 22:39:57 +0900

    update

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app.mount(
 app.add_middleware(
     middleware_class=CORSMiddleware,
     allow_origins=["https://test.varzeny.com"],
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
